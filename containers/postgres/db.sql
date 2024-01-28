644d06a6edfff24e6fff2707304a261e5e0c43cf
Sokratis Valavanis <16580146+sokratisvs@users.noreply.github.com>
2024-01-28 15:37:47 +0200

    db update & users, location API (#3)

--- containers/postgres/db.sql
+++ containers/postgres/db.sql
@@ -1,10 +1,35 @@
 CREATE EXTENSION postgis;
 
-CREATE TABLE geomtable (
-gid serial PRIMARY KEY,
-geom geometry);
+CREATE TABLE users (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP);
 
-INSERT INTO geomtable(geom) VALUES (ST_GeomFromGeoJSON('{"type" : "Polygon",
-"coordinates" : [[[-2.5162124633789067, 34.88787589057115],
-[-2.516899108886719, 34.88696062580312],
-[-2.515665292739868, 34.887752682446184]]]}'));
+-- CREATE IF NOT EXISTS TABLE user_roles (
+--   user_id INT NOT NULL,
+--   role_id INT NOT NULL,
+--   grant_date TIMESTAMP,
+--   PRIMARY KEY (user_id, role_id),
+--   FOREIGN KEY (role_id)
+--       REFERENCES roles (role_id),
+--   FOREIGN KEY (user_id)
+--       REFERENCES accounts (user_id)
+-- );
+
+CREATE TABLE location (
+    user_id INT NOT NULL,
+    gid serial NOT NULL,
+    coordinates geometry(point, 4326),
+    PRIMARY KEY (user_id, gid),
+    FOREIGN KEY (user_id)
+      REFERENCES users (user_id)
+);
+
+-- INSERT INTO geomtable(geom) VALUES (ST_GeomFromGeoJSON('{"type" : "Polygon",
+--     "coordinates" : [[[-2.5162124633789067, 34.88787589057115],
+--     [-2.516899108886719, 34.88696062580312],
+--     [-2.515665292739868, 34.887752682446184]]]}'));
+
--- server/server.js
+++ server/server.js
@@ -2,13 +2,21 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const cors = require('cors')
-const { Client, Pool } = require('pg')
+const bodyParser = require('body-parser')
+const { Pool } = require('pg')
 const dotenv = require('dotenv')
 dotenv.config({ path: '../.env' })
-console.log()
+const usersRoutes = require('./routes/users')
+const locationRoutes = require('./routes/location')
+
 //middleware
 app.use(cors())
 app.use(express.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 const PORT = process.env.SERVER_PORT || 5000
 
@@ -19,6 +27,10 @@ let pool = new Pool({
   user: `${process.env.DB_USER}`,
   password: `${process.env.DB_PASSWORD}`,
 })
+
+app.set('pool', pool)
+app.use('/', usersRoutes)
+app.use('/', locationRoutes)
 
 const textQuery =
   'SELECT ST_AsGeoJSON(geom) FROM geomtable ORDER BY gid DESC LIMIT 1'
@@ -34,22 +46,9 @@ app.get('/geom', async (req, res) => {
   //   await client.end()
 })
 
-// const connectDB = async () => {
-//   try {
-//     console.log('Connect to Postgres ...')
-//     client.connect()
-//     let query
-//     await new Promise(async (resol, rej) => {
-//       query = await client.query(textQuery)
-//     })
-//     console.log('Execution Completed ...', query)
-//   } catch (err) {
-//     console.log('Error while Connecting DB !', err)
-//   }
-//   await client.end()
-// }
-
-// connectToDB()
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
 app.listen(PORT, () => {
   console.log('server is running on port 5000')
